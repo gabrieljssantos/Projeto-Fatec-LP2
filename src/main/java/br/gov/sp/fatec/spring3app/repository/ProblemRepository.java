0e09219545211bc38a50a378e4e7ba00f19b5bda
Gabriel Santos <gabriel.jssantos003@gmail.com>
2024-06-18 04:33:46 +0000

    Remove old-codes

--- src/main/java/br/gov/sp/fatec/spring3app/repository/ProblemRepository.java
+++
@@ -1,8 +0,0 @@
-package br.gov.sp.fatec.spring3app.repository;
-
-import br.gov.sp.fatec.spring3app.entity.Problem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProblemRepository extends JpaRepository<Problem, Long> {
-    boolean existsByProblemCode(String problemCode);
-}
